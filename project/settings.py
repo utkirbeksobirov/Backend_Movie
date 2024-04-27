f3a65680702f1323b72649b2c44a15c16d121362
admin_217 <utkirsobirov2000@gmail.com>
2024-04-26 21:29:36 -0700

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
-import dj_database_url
 import sys
 
 BASE_DIR = Path(__file__).resolve().parent.parent
