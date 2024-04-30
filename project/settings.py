fdb6461f9c074d625cadbfc61a32f81c4aae2fa6
admin_217 <utkirsobirov2000@gmail.com>
2024-04-30 01:23:15 -0700

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -173,7 +173,7 @@ AWS_QUERYSTRING_EXPIRE = 3600
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
